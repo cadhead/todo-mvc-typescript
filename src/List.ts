5d6ec2ca563ee6365e3ff8e5b9601101b9025b7c
OutsiderVsTheWorld <nekorides@gmail.com>
2020-07-18 15:01:48 +0300

    Update List

--- src/List.ts
+++ src/List.ts
@@ -1,6 +1,15 @@
-class TodoList {
-  items: Array<TodoItem> = [];
+import Store from "./Store";
 
+class TodoList {
+  store = new Store();
+
+  addItem(name: string, description: string) {
+    this.store.insert({
+      id: Date.now().toString(),
+      name,
+      description
+    });
+  }
 }
 
 export default TodoList;
