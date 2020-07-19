3e064e0d6c8043377230a1e4c5db20b60614644d
OutsiderVsTheWorld <nekorides@gmail.com>
2020-07-19 13:50:45 +0300

    styling with bulma

--- src/templates/TodoItemTemplate.ts
+++ src/templates/TodoItemTemplate.ts
@@ -1,11 +1,23 @@
-export default (item: TodoItem) => `
-<li data-id="${item.id}"${item.done ? ' class="done"' : ''}>
-	<div>
-		<input type="checkbox" ${item.done ? 'checked' : ''}>
-    <label>
-      <span>${item.name}<span>
-      <span>${item.description}</span>
-    </label>
-		<button class="destroy">x</button>
-	</div>
-</li>`;
+const setClassIfDone = (item: TodoItem, className: string) => {
+  return item.done ? className : '';
+};
+
+export default (item: TodoItem) => `<section class="section">
+<div data-id="${item.id}" class="card ${setClassIfDone(item, 'is-completed')}">
+  <header class="card-header">
+    <p class="card-header-title">${item.name}<p>
+    <button 
+      type="button"
+      class="button button-status card-header-icon ${setClassIfDone(item, 'is-success')}"
+    >
+      <span class="icon">
+        <i class="far fa-square ${setClassIfDone(item, 'fa-check-square')}"></i>
+      </span>
+    </button>
+  </header>
+  <div class="card-content">
+    <div class="content">
+      <p>${item.description}</p>
+    </div>
+  </div>
+</div></section>`;
--- src/templates/TodoListTemplate.ts
+++ src/templates/TodoListTemplate.ts
@@ -1,13 +1,32 @@
 export default () => `
-<section class="container">
+<section class="section container">
   <header>
-    <h1 class="title">Todo List</h1>
-    <p class="subtitle">What needs to be done?</p>
-    <input id="newTodo"/>
+    <div class="hero is-primary">
+      <div class="hero-body">
+        <h1 class="title">Todo List</h1>
+        <p class="subtitle">What needs to be done?</p>
+      </div>
+    </div>
+    <hr class="my-2">
+    <input class="input is-small" placeholder="What you gonna do?" type="text" id="newTodoName"/>
+    <hr class="my-2">
+    <textarea
+      class="textarea"
+      placeholder="Description"
+      type="text"
+      id="newTodoDescription"rows="2"
+    ></textarea>
+    <hr class="my-4">
   </header>
   <main id="todoList">
+    <p
+      class="notification is-primary is-light"
+      id="todoListEmpty">
+        Looks like todo-list is empty. Let's create a first todo!
+    </p>
+    <hr class="my-4">
   </main>
-  <footer class="footer">
+  <footer>
     <p>Enter to add a new todo</p>
     <p>Double-click to edit a todo</p>
   </footer>
